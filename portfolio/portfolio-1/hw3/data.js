7c7cb70d21e80283e3733a40e6d8c5aadb67595b
WeiYen1103 <144879901+WeiYen1103@users.noreply.github.com>
2023-11-11 23:33:53 +0800

    Add files via upload

---
+++ portfolio/portfolio-1/hw3/data.js
@@ -0,0 +1,51 @@
+let evaluation_ratio_1=[
+    {
+        "name": "政府官員",
+        "count": 131
+    },
+    {
+        "name":"民意代表",
+        "count":38
+    },
+    {
+        "name": "學術界人士",
+        "count": 20
+    }
+];
+
+let evaluation_ratio_2=[
+    {
+        "name": "政府官員",
+        "count": 33
+    },
+    {
+        "name":"民意代表",
+        "count":6
+    }
+];
+
+let evaluation_ratio_3=[
+    {
+        "name": "政府官員",
+        "count": 47
+    },
+    {
+        "name":"民意代表",
+        "count":46
+    },
+    {
+        "name": "學術界人士",
+        "count": 13
+    }
+];
+
+let evaluation_ratio_4=[
+    {
+        "name": "政府官員",
+        "count": 44
+    },
+    {
+        "name":"民意代表",
+        "count":32
+    }
+];
